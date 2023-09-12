715fdb58329558ca858670f30aeaed0a2ea4c4e1
joanwongclgmail.com <joanwongcl@gmail.com>
2023-09-12 14:47:27 +0800

    Can't run test, I"m not really sure how to fix it

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int resultOne = add(2,2);
+        System.out.println(resultOne);
+    }
+    public static int add(int numOne, int numTwo) {
+        return numOne+numTwo;
+    }
+
+    public static void main(String[] args) {
+        String resultTwo = greeting("joan");
+        System.out.println(resultTwo);
+    }
+
+    }
+    public static String greeting(String name){
+        return "Bonjour, "+greeting("joan")+"!";
+    }
+
+    public static void main(String[] args) {
+    int resultTHree = add(2,2,3,4);
+        System.out.println(resultTHree);
+    }
+
+    public static int add(int numOne, int numTwo, int numThree, int numFour){
+    int sumOne = add(2,2);
+    int sumTwo = add(numThree 3,4);
+    return add(sumOne,sumTwo);
 }
+
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+
+    public static String main(String printMe) {
+        System.out.println(printMe);
+    }
